b43d0b86eebe8b8f306acedf890843613b505b58
VolviX <20700808+VolviX@users.noreply.github.com>
2024-12-11 08:53:00 +0300

    Fix display function

--- script.js
+++ script.js
@@ -3,11 +3,58 @@ const subtract = (numOne, numTwo) => numOne - numTwo;
 const multiply = (numOne, numTwo) => numOne * numTwo;
 const divide = (numOne, numTwo) => numOne / numTwo;
 
-let firstNum;
-let secondNum;
+function operate(button) {
 
-function operate(operator, inputOne, inputTwo) {
-    return operator(inputOne, inputTwo);
+    if (previousOperand.textContent == '') {
+        previousValue = currentValue;
+    } else {
+
+        if (button.id == 'add') result = previousValue + currentValue;
+        if (button.id == 'subtract') result = previousValue - currentValue;
+        if (button.id == 'multiply') result = previousValue * currentValue;
+        if (button.id == 'divide') result = previousValue / currentValue;
+
+        previousOperand.textContent = result;
+        currentOperand.textContent = 0;
+    }
 }
 
-console.log(operate(divide, 1, 2));
+function display(button) {
+    if (currentOperand.textContent == 0 && button.classList.contains('number')) {
+        currentOperand.textContent = button.textContent;
+        console.log('Number or decimal clicked as first click: ' + button.textContent);
+    } else if (currentOperand.textContent != 0 && button.classList.contains('number')) {
+        currentOperand.textContent += button.textContent;
+        console.log('Number or decimal clicked as non-first click: ' + button.textContent);
+    } else if (previousOperand.textContent == '' && button.classList.contains('operator')) {
+        previousOperand.textContent = currentValue + ' ' + operator.textContent;
+        currentOperand.textContent = 0;
+        console.log('Operator clicked ' + button.textContent);
+    }
+
+}
+
+let currentValue;
+let previousValue;
+let result;
+const currentOperand = document.querySelector('.current.operand');
+const previousOperand = document.querySelector('.previous.operand');
+const number = document.querySelectorAll('.number');
+const operator = document.querySelectorAll('.operator');
+
+// listens numbers and decimal button
+number.forEach(button => {
+    button.addEventListener('click', () => {
+        display(button);
+        currentValue = parseInt(currentOperand.textContent, 10);
+    });
+});
+
+// listens operator buttons
+operator.forEach(button => {
+    button.addEventListener('click', () => {
+        display(button);
+        currentValue = parseInt(currentOperand.textContent, 10);
+        operate(button);
+    });
+});
